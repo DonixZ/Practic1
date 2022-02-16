a3b9066e0965c4e7a3ae2ff4352eaf8d9ef10ae5
danny <dannyzaletsky@gmail.com>
2022-02-16 21:13:16 +0200

    Switch Case

---
+++ src/Prac2.java
@@ -0,0 +1,30 @@
+
+public class Prac2 {
+    public static void main(String[] args) {
+        //Scanner Danny = new Scanner(System.in);
+        //int x = Math.nextInt();
+        //int Num1, Num2, Num3;
+
+        //Num1 = x % 10; //xx!
+        //Num2 = (x % 100) / 10; //x!x
+        //Num3 = x / 100; // !xx
+
+        //System.out.print(Num1 + Num2 + Num3);
+
+        //int x4 = Danny.nextInt();
+        //System.out.print(Math.);
+        // i=0 Beginning of the loop
+        // i>10 from 0-9 (10 loops total)
+        // i++ keep adding 1 more until it reaches 10
+
+        for (int i=1 ; i <= 50; i++){
+            if (i%2 == 1)
+                System.out.println(i);
+        }
+
+        }
+
+
+
+    }
+
