691a631bae3f939e9c36f62e2801794b9ac41306
danny <dannyzaletsky@gmail.com>
2022-02-16 19:07:58 +0200

    Switch Case

--- src/newbCalculator.java
+++ src/newbCalculator.java
@@ -5,40 +5,40 @@ public class newbCalculator {
 
         Scanner input = new Scanner(System.in);
 
-        int num1, num2;
+        int num2;
         char op;
         float result;
 
 
         System.out.print("Please enter the first number: ");
-        num1 = input.nextInt();
+        int num1 = input.nextInt();
         System.out.print("please enter operator: ");
         op = input.next().charAt(0);
         System.out.print("Please enter the second number: ");
         num2 = input.nextInt();
 
         switch (op) {
-            case '+':
+            case '+' -> {
                 result = num1 + num2;
                 System.out.print((int) result);
-                break;
-            case  '-':
+            }
+            case '-' -> {
                 result = num1 - num2;
                 System.out.print((int) result);
-                break;
-            case '*':
+            }
+            case '*' -> {
                 result = num1 * num2;
                 System.out.print((int) result);
-                break;
-            case '/':
-                result = num1 / num2;
+            }
+            case '/' -> {
+                result = (float) num1 / (float) num2;
                 System.out.print(result);
-                break;
-            case '%':
-                result = num1 % num2;
+            }
+            case '%' -> {
+                result = (float) num1 % (float) num2;
                 System.out.print((int) result);
-            default:
-                System.out.print("Invalid Input");
+            }
+            default -> System.out.print("Invalid Input");
         }
     }
 }
